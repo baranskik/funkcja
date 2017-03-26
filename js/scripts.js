fd4fedb22fa64d300afe1642f3dd5980120f870c
Krzysztof Barański <Krzysztof Baraski>
2017-03-27 00:24:17 +0200

    poprawki

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,19 @@
 // plik scripts.js
-var a = 5;
-var h = 10;
-if (a > 0) {
-    console.log('a prawid這we');
-}  else {
-    console.log('nieprawid這we dane');
-  }
-if (h > 0) {
-    console.log('h prawid這we');
-}  else {
-    console.log('nieprawid這we dane');
-  }
-if (a > 0 && h > 0) {
+var a;
+var h;
 function getTriangleArea(a,h) {
-  triangleArea = (a*h/2);
-  return triangleArea;
+  if (a < 0 || h < 0) {
+    return 'Nieprawidlowe dane!';
+  } else {
+    return a * h/2;
+  }
 }
-var TriangleArea = getTriangleArea (a, h);
-console.log('Pole tr鎩k靖a = ' + TriangleArea);
-}
-console.log(getTriangleArea(10,8))
-var Triangle1Area = (getTriangleArea(10,8));
-var Triangle2Area = (getTriangleArea(12,8));
-var Triangle3Area = (getTriangleArea(10,12));
-console.log('Triangle1Area = ' + Triangle1Area)
-console.log('Triangle2Area = ' + Triangle2Area)
-console.log('Triangle3Area = ' + Triangle3Area)
+var triangleArea = getTriangleArea(a,h);
+console.log('Pole tr鎩k靖a = ' + triangleArea)
+console.log(getTriangleArea(10,6))
+var triangle1Area = (getTriangleArea(10,8));
+var triangle2Area = (getTriangleArea(12,8));
+var triangle3Area = (getTriangleArea(10,12));
+console.log('triangle1Area = ' + triangle1Area)
+console.log('triangle2Area = ' + triangle2Area)
+console.log('triangle3Area = ' + triangle3Area)
