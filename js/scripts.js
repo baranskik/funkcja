2ade2f737979c997500ab7cbff58b7ecfc124bda
Krzysztof Barański <Krzysztof Baraski>
2017-03-26 17:05:56 +0200

    funkcja pole trójkąta

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,28 @@
 // plik scripts.js
-
-var a = -20;
-var b = 3;
-var value = ((a * a) + (2 * a * b) - (b * b));
-console.log('Wynik dzialania: ' + value);
-console.log(value > 0 ? 'Wynik dodatni' : 'Wynik ujemny');
+var a = 5;
+var h = 10;
+if (a > 0) {
+    console.log('a prawid這we');
+}  else {
+    console.log('nieprawid這we dane');
+  }
+if (h > 0) {
+    console.log('h prawid這we');
+}  else {
+    console.log('nieprawid這we dane');
+  }
+if (a > 0 && h > 0) {
+function getTriangleArea(a,h) {
+  triangleArea = (a*h/2);
+  return triangleArea;
+}
+var TriangleArea = getTriangleArea (a, h);
+console.log('Pole tr鎩k靖a = ' + TriangleArea);
+}
+console.log(getTriangleArea(10,8))
+var Triangle1Area = (getTriangleArea(10,8));
+var Triangle2Area = (getTriangleArea(12,8));
+var Triangle3Area = (getTriangleArea(10,12));
+console.log('Triangle1Area = ' + Triangle1Area)
+console.log('Triangle2Area = ' + Triangle2Area)
+console.log('Triangle3Area = ' + Triangle3Area)
